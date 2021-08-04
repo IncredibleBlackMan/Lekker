353e354371a29e93c56fb6b7a26d8e7083747543
IncredibleBlackMan <ramonomondi@gmail.com>
2021-08-05 02:07:45 +0300

    handle archive and unarchive of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,48 @@
 class UsersController < ApplicationController
+  include JSONAPI::Errors
+
+  before_action :find_user_data, except: %i[index]
+  after_action :save_status_management, except: %i[index], if: -> { response.status == 200 }
+
   def index
-    render jsonapi: User.all
+    render jsonapi: User.filter_by_status(params[:status])
+  end
+
+  def archive
+    authorize @user_data
+
+    @user_data.archive!
+
+    if @user_data.save
+      render jsonapi: @user_data, status: :ok
+    else
+      render jsonapi_errors: @user_data.errors, status: :unprocessable_entity
+    end
+  rescue AASM::InvalidTransition => e
+    render json: { errors: e.message }, status: :unprocessable_entity
+  end
+
+  def unarchive
+    authorize @user_data
+
+    @user_data.unarchive!
+
+    if @user_data.save
+      render jsonapi: @user_data, status: :ok
+    else
+      render jsonapi_errors: @user_data.errors, status: :unprocessable_entity
+    end
+  rescue AASM::InvalidTransition => e
+    render json: { errors: e.message }, status: :unprocessable_entity
+  end
+
+  private
+
+  def save_status_management
+    StatusManagement.create_status_management(current_user, @user_data, params[:action])
+  end
+
+  def find_user_data
+    @user_data = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,34 @@
 class User < ApplicationRecord
+  include AASM
+
+  aasm column: 'status' do
+    state :active, initial: true
+    state :archived
+
+    event :archive, after_commit: :send_status_notification do
+      transitions from: :active, to: :archived
+    end
+
+    event :unarchive, after_commit: :send_status_notification do
+      transitions from: :archived, to: :active
+    end
+  end
+
   has_secure_password
+
+  has_many :status_managements
 
   validates :email,
             presence: true,
             uniqueness: true
+
+  scope :filter_by_status, proc { |status|
+    where(status: status) if status.present?
+  }
+
+  def send_status_notification
+    UserMailer.user_status_notification(self).deliver_now
+  end
+
+  private
 end
